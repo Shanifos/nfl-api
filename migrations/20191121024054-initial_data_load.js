d6df44197cb9d76cdcb71bf494b26c2b3972791b
Shanifos <Shanifoshee@gmail.com>
2019-11-22 13:42:55 -0500

    recommit

--- migrations/20191121024054-initial_data_load.js
+++ migrations/20191121024054-initial_data_load.js
@@ -260,23 +260,9 @@ module.exports = {
         division: 'West'
       }
     ])
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
   },
 
   down: async (queryInterface, Sequelize) => {
     return await queryInterface.bulkDelete('teams')
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
   }
 }
